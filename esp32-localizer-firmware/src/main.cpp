28c7be05fdd0dcd2c63afcdbe0a674ebd5730d1f
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-05-23 08:59:04 +0200

    feat(fw): log mac address

--- esp32-localizer-firmware/src/main.cpp
+++ esp32-localizer-firmware/src/main.cpp
@@ -1,3 +1,7 @@
+#include <Arduino.h>
+#include <ArduinoLog.h>
+#include <WiFi.h>
+
 #include "config.h"
 #include "util.h"
 #include "setup_logging.h"
@@ -9,8 +13,11 @@ void setup()
 {
   Serial.begin(SERIAL_BAUD_RATE);
   setupLogging(LOG_LEVEL_VERBOSE, &Serial);
+  Log.infoln("Starting initialization");
+  Log.infoln("My MAC address is: %s", WiFi.macAddress().c_str());
   delay(INITIAL_DELAY); // wait for serial and WiFi to stabilize
   setupWiFi();
+  Log.infoln("Initalized");
 }
 
 void loop()
